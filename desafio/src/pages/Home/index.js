f9fe3eb4a5efdba9a00def096052592d7bacfa68
marcosaureliodiasmoura <marcos_moura@live.com>
2022-08-01 22:57:52 -0300

    feat: create api connection

--- desafio/src/pages/Home/index.js
+++ desafio/src/pages/Home/index.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    Row,
-    Col
-  } from 'react-bootstrap';
-  import { Container, BoxProducts } from './styles';
+  Row,
+  Col
+} from 'react-bootstrap';
+import { Container, BoxProducts } from './styles';
 
-  //components
-  import Header from '../../components/Header'
-  import Product from '../../components/Product'
+//components
+import Header from '../../components/Header'
+import Product from '../../components/Product'
 
-  export default function Home() {
-      return(
-        <>
-          <Container>
-            <Row>
-              <Col xs={12}>
-                <Header />
-              </Col>
-              <h2>Produtos</h2>
-              <BoxProducts>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-              </BoxProducts>  
-            </Row>
-          </Container>
-        </>
-      );
-  }
+export default function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://62d742f351e6e8f06f1a83da.mockapi.io/api/produtos')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, []);
+
+  console.log(products);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <Header />
+          </Col>
+          <h2>Produtos</h2>
+          <BoxProducts>
+            <Product props />
+            <Product props />
+            <Product props />
+            <Product props />
+            <Product props />
+            <Product props />
+          </BoxProducts>
+        </Row>
+      </Container>
+    </>
+  );
+}
