b8225d20b2f0d567fc352968ec14986f2b6e19b7
marcosaureliodiasmoura <marcos_moura@live.com>
2022-08-02 15:29:38 -0300

    feat: create filter products

--- desafio/src/components/Search/index.js
+++ desafio/src/components/Search/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   Form
 } from 'react-bootstrap';
+import { ProductContext } from '../../hooks/product';
 
 export default function Search(props) {
+  const { products, setProducts } = useContext(ProductContext);
+
+  const [filterProduct, setFilterProduct] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    if (search.length) {
+      setFilterProduct(products.filter(p => p.name
+        .toLowerCase()
+        .includes(search.toLowerCase())))
+    }
+
+    if(search.length > 0){
+      setProducts(filterProduct); 
+    }
+  }, [search]);
+
+  console.log(filterProduct)
+
+
   return (
     <>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>{props.title}</Form.Label>
-          <Form.Control type="text" onChange={props.filterName} placeholder="Buscar produto"/>
+          <Form.Control
+            type="text"
+            placeholder="Buscar produto"
+            onChange={e => setSearch(e.target.value)}
+            value={search}
+          />
         </Form.Group>
       </Form>
     </>
